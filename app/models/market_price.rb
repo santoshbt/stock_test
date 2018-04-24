f1f52d155a93c614724eb8dd643c0eb5d5e7217a
santoshbt <bt.santosh01@gmail.com>
2018-04-24 23:52:34 +0530

    added marketprice validations

--- app/models/market_price.rb
+++ app/models/market_price.rb
@@ -1,3 +1,7 @@
 class MarketPrice < ApplicationRecord
   has_many :stocks
+
+  validates :currency, :value_cents, presence: true
+  validates :value_cents, numericality: { only_integer: true }
+  validates :currency, uniqueness: { scope: :value_cents, message: "Value cannot be same for more than one currency." }
 end
--- spec/models/bearer_spec.rb
+++ spec/models/bearer_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Bearer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @bearer = FactoryGirl.create(:bearer)
+  end
+
+  describe "Validation" do
+    it "can be created" do
+      expect(@bearer).to be_valid
+    end
+
+    it "has a valid name" do
+      @bearer.name = "Tina"
+      expect(@bearer).to be_valid
+    end
+
+    it "cannot be created without a valid name" do
+      @bearer.name = nil
+      expect(@bearer).to_not be_valid
+    end
+  end
+
+  describe "Association" do
+    describe Bearer do
+      it "has many stocks" do
+        should have_many(:stocks)
+      end
+    end
+  end
 end
